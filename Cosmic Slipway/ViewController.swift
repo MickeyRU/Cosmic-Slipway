918e85a9bb90a0a975d03fb092fbccb8597bb8e1
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-03-25 14:55:38 +0300

    Initial Commit

---
+++ Cosmic Slipway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cosmic Slipway
+//
+//  Created by Павел Афанасьев on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
