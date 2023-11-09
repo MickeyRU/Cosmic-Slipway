f996a7ff2d6ec9f1671291a54d50c11edd0aa50a
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-11-09 17:44:03 +0300

    Добавлена рамка в HeaderView

--- Cosmic Slipway/DesignSystem/Components/HeaderView/HeaderView.swift
+++ Cosmic Slipway/DesignSystem/Components/HeaderView/HeaderView.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Combine
 
-final class HeaderView: UIView {
+final class HeaderView: UIView, ShadowConfigurable, BorderConfigurable {
     
     // MARK: - Combine Subjects
     
@@ -51,6 +51,29 @@ final class HeaderView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addShadow(to: self.okButton, 
+                  cornerRadius: 28,
+                  shadowColor: HighlightsColors.pureBlack,
+                  shadowOpacity: 0.8,
+                  shadowOffset: CGSize(width: 0, height: -4),
+                  shadowRadius: 16)
+        addShadow(to: self.exitButton, 
+                  cornerRadius: 28,
+                  shadowColor: HighlightsColors.pureBlack,
+                  shadowOpacity: 0.8,
+                  shadowOffset: CGSize(width: 0, height: -4),
+                  shadowRadius: 16)
+        addBorder(to: self.bgImage,
+                  cornerRadius: 28,
+                  lineWidth: 1,
+                  borderColors: [HighlightsColors.pureBlack,
+                                 HighlightsColors.pureBlack],
+                  startPoint: CGPoint(x: 0, y: 0),
+                  endPoint: CGPoint(x: 1, y: 1))
     }
     
     // MARK: Private methods
